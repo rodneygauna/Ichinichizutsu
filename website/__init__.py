f53a7d1779815e6f080e5ef7f63f389cadc6b3ba
Rodney Gauna <rodneygauna@gmail.com>
2022-11-29 23:55:53 -0800

    Start of the app

---
+++ website/__init__.py
@@ -0,0 +1,53 @@
+from flask import Flask, redirect
+from flask.helpers import url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from dotenv import load_dotenv
+import os
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
+load_dotenv()
+
+SECRET_KEY = os.getenv('SECRET_KEY')
+
+def create_app():
+    '''Initializes the application using Flask'''
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = SECRET_KEY
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
+
+    # Import views and auth routes
+    # from website.core.view import core
+
+    # Blueprint routes
+    #app.register_blueprint(core)
+
+    # Check is database exists; if not, create database and tables
+    # from .models import User
+    # create_database(app)
+
+    # Initializes the login manager
+    login_manager = LoginManager()
+    login_manager.login_view = 'users.login'
+    login_manager.login_message = u'You have to login to view this page.'
+    login_manager.login_message_category = 'warning'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
+
+    @login_manager.unauthorized_handler
+    def unauthorized():
+        return redirect(url_for('users.login'))
+
+    return app
+
+def create_database(app):
+    '''Creates the database'''
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print('Created database!')
