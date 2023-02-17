208c322723575a97af8bd3d2e2f12365ac91e033
Rodney Gauna <rodneygauna@gmail.com>
2023-02-16 23:48:41 -0800

    start of the dictory list

---
+++ src/dict.py
@@ -0,0 +1,16 @@
+'''
+List of all the dictionaries used in the application
+'''
+
+TODOS_STATUS = [
+    ('Not Started', 'Not Started'),
+    ('In Progress', 'In Progress'),
+    ('Completed', 'Completed'),
+    ('On Hold', 'On Hold'),
+    ('Archived', 'Archived')
+]
+
+STATUS = [
+    ('Active', 'Active'),
+    ('Inactive', 'Inactive')
+]
