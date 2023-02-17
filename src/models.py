068f0a76735fd3dd41d6f4011325c865493392b9
Rodney Gauna <rodneygauna@gmail.com>
2023-02-16 23:48:29 -0800

    addition of the todo table model

--- src/models.py
+++ src/models.py
@@ -36,3 +36,22 @@ class Users(db.Model, UserMixin):
 
     def __repr__(self):
         return f"Email: {self.email}"
+
+
+class Todos(db.Model):
+    '''SQL Table: todos'''
+    __tablename__ = 'todos'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    title = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    status = db.Column(db.String(20), nullable=False, default='Not Started')
+    created_at = db.Column(
+        db.DateTime(timezone=True), nullable=False, default=datetime.utcnow)
+    updated_at = db.Column(
+        db.DateTime(timezone=True), nullable=False, default=datetime.utcnow)
+    # Relationships
+    user = db.relationship('Users', backref=db.backref('todos'), lazy=True)
+
+    def __repr__(self):
+        return f"Title: {self.title}"
